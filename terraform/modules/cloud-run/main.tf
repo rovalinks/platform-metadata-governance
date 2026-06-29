ef0e6bf1bcd13adbd332ea088d2454bf4a9acf87
Rohith <rohith.singirikonda@rovalinks.com>
2026-06-29 20:36:42 +0100

    updated Registry Validation yaml

--- terraform/modules/cloud-run/main.tf
+++ terraform/modules/cloud-run/main.tf
@@ -1,25 +1,31 @@
 resource "google_cloud_run_v2_service" "this" {
-
   name     = var.service_name
   project  = var.project_id
   location = var.region
 
-  ingress = "INGRESS_TRAFFIC_INTERNAL_LOAD_BALANCER"
+  # Set ingress to allow all traffic
+  ingress = "INGRESS_TRAFFIC_ALL"
+
+  # Enable deletion protection to prevent accidental removal
+  deletion_protection = true
 
   template {
-
     service_account = var.service_account_email
 
     containers {
-
       image = var.image
 
       ports {
         container_port = 8080
       }
 
+      # Add resource limits for CPU and Memory
+      resources {
+        limits = {
+          cpu    = "1"
+          memory = "512Mi"
+        }
+      }
     }
-
   }
-
 }
