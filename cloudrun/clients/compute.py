2ab6ccbbb4c8d1159f4d0e6ecd2f1977eeedf424
Rohith <rohith.singirikonda@rovalinks.com>
2026-06-30 09:55:34 +0100

    Add Compute Engine client for metadata enforcement

--- cloudrun/clients/compute.py
+++ cloudrun/clients/compute.py
@@ -1,12 +1,18 @@
+from google.cloud import compute_v1
+
 from clients.base import ResourceClient
 
 
 class ComputeClient(ResourceClient):
 
-    def supports(self, asset_type: str) -> bool:
+    def __init__(self):
+
+        self.instances = compute_v1.InstancesClient()
+
+    def supports(self, asset_type: str):
 
         return asset_type == "compute.googleapis.com/Instance"
 
     def apply_labels(self, resource, labels: dict):
 
-        raise NotImplementedError()
+        raise NotImplementedError("Implementation added in next step.")
