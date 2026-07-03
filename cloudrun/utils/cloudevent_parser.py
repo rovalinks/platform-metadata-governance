4181117abea534dff3f6a34aa176f17b98d02123
rovalinks <rohith.singirikonda@rovalinks.com>
2026-07-03 22:25:55 +0100

    updated generic cloud event parser

--- cloudrun/utils/cloudevent_parser.py
+++ cloudrun/utils/cloudevent_parser.py
@@ -1,4 +1,5 @@
 from models.audit_log_event import AuditLogEvent
+from utils.logger import logger
 
 
 class CloudEventParser:
@@ -32,6 +33,50 @@ class CloudEventParser:
             "",
         )
 
+        #
+        # Cloud SQL create events sometimes emit only the
+        # project as the resource name. Attempt to build
+        # the full instance resource.
+        #
+        if (
+            service_name == "cloudsql.googleapis.com"
+            and resource_name.count("/") == 1
+        ):
+
+            response = payload.get(
+                "response",
+                {}
+            )
+
+            request = payload.get(
+                "request",
+                {}
+            )
+
+            instance = (
+                response.get("name")
+                or response.get("instance")
+                or response.get("targetId")
+                or request.get("name")
+                or request.get("instance")
+            )
+
+            if instance:
+
+                resource_name = (
+                    f"{resource_name}/instances/{instance}"
+                )
+
+                logger.info(
+                    "Normalized Cloud SQL resource to %s",
+                    resource_name,
+                )
+            else:
+
+                logger.warning(
+                    "Unable to determine Cloud SQL instance name."
+                )
+
         resource = event.get(
             "resource",
             {}
@@ -43,13 +88,18 @@ class CloudEventParser:
         )
 
         return AuditLogEvent(
+
             service_name=service_name,
+
             method_name=method_name,
+
             resource_name=resource_name,
+
             project_id=labels.get(
                 "project_id",
                 "",
             ),
+
             location=labels.get(
                 "location",
             ),
