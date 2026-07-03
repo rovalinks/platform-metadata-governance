de8d151691c086f7d381838473af73652f632c3c
rovalinks <rohith.singirikonda@rovalinks.com>
2026-07-03 21:03:29 +0100

    Implemented storage trigger support & corrected

--- cloudrun/clients/storage.py
+++ cloudrun/clients/storage.py
@@ -1,33 +1,43 @@
-from google.cloud import storage
 import logging
+
+from google.cloud import storage
 
 from clients.base import ResourceClient
 from config import EXCLUDED_BUCKETS
+from models.resource import Resource
 
 # Configure logger
 logger = logging.getLogger(__name__)
 
-class StorageClient(ResourceClient):
 
+class StorageClient(ResourceClient):
     """Cloud Storage resource adapter."""
 
     def __init__(self):
 
         self.client = storage.Client()
 
-    def supports(self, asset_type: str):
+    def supports(
+        self,
+        asset_type: str,
+    ):
 
         return asset_type == "storage.googleapis.com/Bucket"
 
-    def labels(self, resource):
+    def labels(
+        self,
+        resource,
+    ):
 
         bucket_name = resource.name.split("/")[-1]
 
         if bucket_name in EXCLUDED_BUCKETS:
+
             logger.info(
                 "Skipping excluded bucket %s",
                 bucket_name,
             )
+
             return {}
 
         bucket = self.client.get_bucket(
@@ -38,15 +48,54 @@ class StorageClient(ResourceClient):
             bucket.labels or {}
         )
 
-    def apply_labels(self, resource, labels: dict):
+    def get(
+        self,
+        resource_name: str,
+    ) -> Resource:
+        """
+        Retrieves a Cloud Storage bucket and returns
+        the platform Resource model.
+        """
+
+        bucket_name = resource_name.split("/")[-1]
+
+        bucket = self.client.get_bucket(
+            bucket_name
+        )
+
+        return Resource(
+
+            asset_type="storage.googleapis.com/Bucket",
+
+            name=resource_name,
+
+            project="",
+
+            location=bucket.location,
+
+            labels=dict(
+                bucket.labels or {}
+            ),
+
+            tags={},
+
+        )
+
+    def apply_labels(
+        self,
+        resource,
+        labels: dict,
+    ):
 
         bucket_name = resource.name.split("/")[-1]
 
         if bucket_name in EXCLUDED_BUCKETS:
+
             logger.info(
                 "Skipping excluded bucket %s",
                 bucket_name,
             )
+
             return False
 
         bucket = self.client.get_bucket(
