24f3d8bcb73742628f683642417352e9018baa0b
rovalinks <rohith.singirikonda@rovalinks.com>
2026-07-03 21:49:14 +0100

    Implemented bigquery util support updated

--- cloudrun/utils/bigquery.py
+++ cloudrun/utils/bigquery.py
@@ -1,8 +1,28 @@
-def parse_dataset_name(resource_name: str):
+def parse_dataset_name(
+    resource_name: str,
+):
+    """Parses a BigQuery dataset resource name."""
 
+    #
+    # Brownfield:
+    # //bigquery.googleapis.com/projects/<project>/datasets/<dataset>
+    #
+    if resource_name.startswith("//"):
+
+        parts = resource_name.split("/")
+
+        return {
+            "project": parts[4],
+            "dataset": parts[6],
+        }
+
+    #
+    # Greenfield:
+    # projects/<project>/datasets/<dataset>
+    #
     parts = resource_name.split("/")
 
     return {
-        "project": parts[4],
-        "dataset": parts[6],
+        "project": parts[1],
+        "dataset": parts[3],
     }
