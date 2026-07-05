c04f5afa0b59ff34048f738b6b310300ba29ed6b
rovalinks <rohith.singirikonda@rovalinks.com>
2026-07-05 12:45:24 +0100

    feat(remediation): updated dispatcher

--- cloudrun/dispatcher.py
+++ cloudrun/dispatcher.py
@@ -12,6 +12,7 @@ from handlers.history import history
 from handlers.dashboard import dashboard
 from handlers.metrics import metrics
 from handlers.greenfield import greenfield
+from handlers.worker import worker
 
 class Dispatcher:
 
@@ -63,6 +64,9 @@ class Dispatcher:
         if route == "greenfield":
             return greenfield(payload)
 
+        if route == "worker":
+            return worker()
+            
         return {
             "error": "Endpoint not found"
         }, 404
