9e3e35fa93c8926dc20f0e1b5b8236f4067d7934
rovalinks <rohith.singirikonda@rovalinks.com>
2026-07-05 08:57:25 +0100

    feat(compute): add parsers for instance templates and managed instance groups

--- cloudrun/utils/compute.py
+++ cloudrun/utils/compute.py
@@ -64,3 +64,37 @@ def parse_forwarding_rule_name(resource_name: str):
         "region": parts[6],
         "forwarding_rule": parts[8],
     }
+
+def parse_instance_template_name(resource_name: str):
+    """Parses a Compute Instance Template."""
+
+    if not resource_name.startswith("//"):
+        resource_name = (
+            "//compute.googleapis.com/"
+            + resource_name
+        )
+
+    parts = resource_name.split("/")
+
+    return {
+        "project": parts[4],
+        "instance_template": parts[6],
+    }
+
+
+def parse_instance_group_manager_name(resource_name: str):
+    """Parses a Compute Instance Group Manager."""
+
+    if not resource_name.startswith("//"):
+        resource_name = (
+            "//compute.googleapis.com/"
+            + resource_name
+        )
+
+    parts = resource_name.split("/")
+
+    return {
+        "project": parts[4],
+        "zone": parts[6],
+        "instance_group_manager": parts[8],
+    }
