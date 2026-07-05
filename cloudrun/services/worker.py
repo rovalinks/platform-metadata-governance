90fc1facb27c97129f65f4bcc62d72134a80e779
rovalinks <rohith.singirikonda@rovalinks.com>
2026-07-05 12:44:20 +0100

    Added worker services

---
+++ cloudrun/services/worker.py
@@ -0,0 +1,24 @@
+from services.executor import ExecutorService
+
+
+class WorkerService:
+    """
+    Executes one Cloud Tasks remediation batch.
+    """
+
+    def __init__(self):
+
+        self.executor = ExecutorService()
+
+    def execute(
+        self,
+        run_id: str,
+        offset: int,
+        batch_size: int,
+    ):
+
+        return self.executor.execute_batch(
+            run_id=run_id,
+            offset=offset,
+            batch_size=batch_size,
+        )
