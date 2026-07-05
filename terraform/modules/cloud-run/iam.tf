de97ac4f847e74069d6844558ab76ea2260f7ecb
rovalinks <rohith.singirikonda@rovalinks.com>
2026-07-05 08:55:49 +0100

    added IAM security

--- terraform/modules/cloud-run/iam.tf
+++ terraform/modules/cloud-run/iam.tf
@@ -9,3 +9,23 @@ resource "google_cloud_run_v2_service_iam_member" "eventarc_invoker" {
   member = "serviceAccount:${var.service_account_email}"
 
 }
+
+resource "google_cloud_run_v2_service_iam_member" "developer_invoker" {
+
+  project  = var.project_id
+  location = var.region
+  name     = google_cloud_run_v2_service.this.name
+
+  role = "roles/run.invoker"
+
+  member = "user:rohith555raju@gmail.com"
+}
+
+
+# For a customer deployment, nee to  replace:
+
+# member = "user:rohith555raju@gmail.com"
+# with
+# member = "domain:customer.com"
+# or preferably
+# member = "group:platform-engineers@customer.com"
