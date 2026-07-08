584b127a281eff2189352cb81593a2076db506d1
rovalinks <rohith.singirikonda@rovalinks.com>
2026-07-08 12:40:46 +0100

    updated api key support

--- cloudrun/clients/apikeys.py
+++ cloudrun/clients/apikeys.py
@@ -1,7 +1,5 @@
 from google.cloud import api_keys_v2
 
-import config
-
 from clients.base import ResourceClient
 from models.resource import Resource
 from utils.apikeys import parse_key_name
@@ -12,110 +10,31 @@ class ApiKeysClient(ResourceClient):
     """
 
     def __init__(self):
-
         self.client = api_keys_v2.ApiKeysClient()
 
-    def supports(
-        self,
-        asset_type: str,
-    ):
+    def supports(self, asset_type: str):
+        return asset_type == "apikeys.googleapis.com/Key"
 
-        return (
-            asset_type
-            == "apikeys.googleapis.com/Key"
-        )
+    def labels(self, resource):
+        return {}
 
-    def labels(
-        self,
-        resource,
-    ):
-
-        key = self.client.get_key(
-            name=parse_key_name(resource.name)
-        )
-
-        return dict(
-            key.labels or {}
-        )
-
-    def get(
-        self,
-        resource_name: str,
-    ) -> Resource:
-
+    def get(self, resource_name: str) -> Resource:
         key = self.client.get_key(
             name=parse_key_name(resource_name)
         )
 
-        project = (
-            resource_name.split("/")[3]
-        )
+        project = resource_name.split("/")[3]
 
         return Resource(
-
             asset_type="apikeys.googleapis.com/Key",
-
             name=resource_name,
-
             project=project,
-
             location="global",
-
-            labels=dict(
-                key.labels or {}
-            ),
-
+            labels={},
             tags={},
-
         )
 
-    def apply_labels(
-        self,
-        resource,
-        labels: dict,
-    ):
-
-        key = self.client.get_key(
-            name=parse_key_name(resource.name)
+    def apply_labels(self, resource, labels: dict):
+        raise NotImplementedError(
+            "API Keys do not support labels."
         )
-
-        existing = dict(
-            key.labels or {}
-        )
-
-        if config.PRESERVE_EXISTING_LABELS:
-
-            merged = existing.copy()
-
-            for k, v in labels.items():
-
-                if k not in merged:
-
-                    merged[k] = v
-
-        else:
-
-            merged = existing.copy()
-            merged.update(labels)
-
-        if merged == existing:
-
-            return True
-
-        key.labels = merged
-
-        operation = self.client.update_key(
-
-            key=key,
-
-            update_mask={
-                "paths": [
-                    "labels",
-                ]
-            },
-
-        )
-
-        operation.result()
-
-        return True
